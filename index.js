63871d4ebb08f9201822cae54c3c213b5399f792
Stvlutta <stvlutta@gmail.com>
2025-03-03 11:49:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// index.js
 
-// Write your code here
+// Define num1 and num2 for the multiply test
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Define random for the random test
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 for the mod test
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define max for the max test
+const max = Math.max(1, 5, 10, 15, 20);
